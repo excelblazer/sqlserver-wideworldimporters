6545847b88654e7228b9f4c508b5557e419b75e8
Roshan <155152933+excelblazer@users.noreply.github.com>
2025-04-29 19:59:39 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Create backup folder if not exists
+mkdir -p /var/opt/mssql/backup
+
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
 sleep 20
 
-# Download WideWorldImporters backup file
+# Download backup file
 curl -L -o /var/opt/mssql/backup/WideWorldImporters-Full.bak \
   https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
 
-# Restore the database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'dummy@123' -Q \
+# Restore database
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'YourStrong@Passw0rd' -Q \
 "RESTORE DATABASE WideWorldImporters FROM DISK = '/var/opt/mssql/backup/WideWorldImporters-Full.bak' WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf', MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf'"
